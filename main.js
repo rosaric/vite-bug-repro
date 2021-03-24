3b71fe72f517b217fe2f160474993d0c05d052e5
Pig Fang <g-plane@hotmail.com>
2021-03-24 17:58:54 +0800

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+const code = 'export const val = 123'
+
+// create a blob
+const blob = new Blob([code], { type: 'application/ecamscript;charset=UTF-8' })
+
+// create URL based on the blob above
+const url = URL.createObjectURL(blob)
+console.info(url)
+
+;(async () => {
+  await import(/* @vite-ignore */url) // <== Vite modify this URL, check browser console for detail
+})()
